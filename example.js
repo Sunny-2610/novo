955044da1998b2a9d3793bbc33c2f1a9f595e1cd
Sunny-2610 <sunnysinha9884@gmail.com>
2026-03-28 13:22:17 +0530

    learned about the http core module in it

--- example.js
+++ example.js
@@ -1,4 +1,6 @@
 const fs = require('fs')
+const http = require('http')
+
 
 // fs.readFile('example.txt' , 'utf8' ,(err,data) =>{
 //     if (err) {
@@ -9,12 +11,22 @@ const fs = require('fs')
 // })    
 
 
-const content = 'hello world'
+// const content = 'hello world'
 
-fs.writeFile('output.txt',content,(err) =>{
-    if (err) {
-        console.log(err)
-        return;
-    }
-    console.log('written in new file successfully')
+// fs.writeFile('output.txt',content,(err) =>{
+//     if (err) {
+//         console.log(err)
+//         return;
+//     }
+//     console.log('written in new file successfully')
+// })
+
+const server = http.createServer((req,res)=>{
+    res.statusCode = 200
+    res.setHeader('Content-Type','text/plain')
+    res.end('hello world')
+})
+
+server.listen(3000,()=>{
+    console.log('server running at http://localhost:3000')
 })
