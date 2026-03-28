666ee54f1eb95b5a7129f4a37358ab869304f701
Sunny-2610 <sunnysinha9884@gmail.com>
2026-03-28 13:45:19 +0530

    completed the URl module

--- example.js
+++ example.js
@@ -1,42 +1,62 @@
-const fs = require('fs')
-const http = require('http')
-const path = require('path')
-const os = require('os')
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+const os = require('os');
+const { URL } = require('url'); // ✅ fixed import
 
-// fs.readFile('example.txt' , 'utf8' ,(err,data) =>{
+
+// ---------- Read File ----------
+// fs.readFile('example.txt', 'utf8', (err, data) => {
 //     if (err) {
-//         console.log(err)
+//         console.log(err);
 //         return;
 //     }
-//     console.log(data)
-// })    
+//     console.log(data);
+// });
 
 
-// const content = 'hello world'
+// ---------- Write File ----------
+// const content = 'hello world';
 
-// fs.writeFile('output.txt',content,(err) =>{
+// fs.writeFile('output.txt', content, (err) => {
 //     if (err) {
-//         console.log(err)
+//         console.log(err);
 //         return;
 //     }
-//     console.log('written in new file successfully')
-// })
-
-// const server = http.createServer((req,res)=>{
-//     res.statusCode = 200
-//     res.setHeader('Content-Type','text/plain')
-//     res.end('hello world')
-// })
-
-// server.listen(3000,()=>{
-//     console.log('server running at http://localhost:3000')
-// })
+//     console.log('written in new file successfully');
+// });
 
 
-// const directory = '/user/local'
-// const filename = 'example.txt'
-// const fullpath = path.join(directory,filename)
-// console.log(fullpath)
+// ---------- Create Server ----------
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('hello world');
+// });
 
+// server.listen(3000, () => {
+//     console.log('server running at http://localhost:3000');
+// });
+
+
+// ---------- Path Module ----------
+// const directory = '/user/local';
+// const filename = 'example.txt';
+// const fullpath = path.join(directory, filename);
+// console.log(fullpath);
+
+
+// ---------- OS Module ----------
 // console.log('Platform:', os.platform());
 // console.log('Arch:', os.arch());
+
+
+// ---------- URL Module (FIXED) ----------
+const myUrl = new URL('http://example.com:8080/path/name?query=hello#hash');
+
+console.log('Host:', myUrl.host);
+console.log('Hostname:', myUrl.hostname);
+console.log('Port:', myUrl.port);
+console.log('Pathname:', myUrl.pathname);
+console.log('Query:', myUrl.search);
+console.log('Hash:', myUrl.hash);
