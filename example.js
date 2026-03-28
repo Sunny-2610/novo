0b4fb4d4090d21255ea3431019cbbe959088e04d
Sunny-2610 <sunnysinha9884@gmail.com>
2026-03-28 13:11:00 +0530

    learn about the core module which is realated to fs

--- example.js
+++ example.js
@@ -1,1 +1,20 @@
- 
+const fs = require('fs')
+
+// fs.readFile('example.txt' , 'utf8' ,(err,data) =>{
+//     if (err) {
+//         console.log(err)
+//         return;
+//     }
+//     console.log(data)
+// })    
+
+
+const content = 'hello world'
+
+fs.writeFile('output.txt',content,(err) =>{
+    if (err) {
+        console.log(err)
+        return;
+    }
+    console.log('written in new file successfully')
+})
